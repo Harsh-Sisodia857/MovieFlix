26e15f7ddf1656c1894e0a5bd95118acf65cd778
Harsh-Sisodia857 <92028641+Harsh-Sisodia857@users.noreply.github.com>
2023-02-03 18:55:32 +0530

    Make Simple Table of Movies using dummy data and using pagination

---
+++ src/component/common/like.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+// Input : liked : boolean
+// Output : onClick
+
+class Like extends Component {
+    render() { 
+        let classes = "fa fa-heart";
+        if (!this.props.liked)
+            classes += "-o"
+        return (
+            <i onClick={ this.props.onClick } style = {{cursor : "pointer"}} className={classes}></i>
+        );
+    }
+}
+ 
+export default Like;
+
+// stateless functional component
+
+// const Like = (props) => {
+//    let classes = "fa fa-heart";
+//         if (!props.liked)
+//             classes += "-o"
+//         return (
+//             <i onClick={ props.onClick } style = {{cursor : "pointer"}} className={classes}></i>
+//         );
+// }
+ 
+// export default Like;
